ba15d56568966fc89fd1711eb677281aab2dd40f
UserFunctionXXX <renangomes09@hotmail.com>
2022-06-08 14:26:29 -0300

    Estrutura inicial para uso do Flutter Modular

---
+++ lib/app/app_module.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'modules/splash/splash_page.dart';
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds => const [];
+
+  @override
+  List<ModularRoute> get routes =>
+      [ChildRoute('/', child: (context, args) => const SplashPage())];
+}
---
+++ lib/app/core/ui/app_config_ui.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppConfigUI {
+  AppConfigUI._(); //Não deixa instanciar classe
+
+  static final ThemeData theme = ThemeData(
+    primaryColor: const Color(0XFF006680),
+    primaryColorLight: const Color(0XFF219FFF),
+  );
+}
