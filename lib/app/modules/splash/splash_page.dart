2b787fca466085ee7ce791a1fe09a719cd439d0a
UserFunctionXXX <renangomes09@hotmail.com>
2022-06-16 22:33:56 -0300

    Melhorias na Splash SCREEN

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -5,32 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:job_timer/app/core/ui/job_timer_icons.dart';
 
-class SplashPage extends StatefulWidget {
+class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
-  State<SplashPage> createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(Duration(seconds: 3), () => _navigateUserAuth());
-  }
-
-  void _navigateUserAuth() {
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user == null) {
-        Modular.to.navigate('/login');
-      } else {
-        Modular.to.navigate('/home');
-      }
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 1), () => _navigateUserAuth());
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -54,5 +34,15 @@ class _SplashPageState extends State<SplashPage> {
         ),
       ),
     );
+  }
+
+  void _navigateUserAuth() {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        Modular.to.navigate('/login/');
+      } else {
+        Modular.to.navigate('/home/');
+      }
+    });
   }
 }
