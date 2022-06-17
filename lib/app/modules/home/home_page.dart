abdfdd097861af8025eec83e61ebe1277dab2dea
UserFunctionXXX <renangomes09@hotmail.com>
2022-06-16 22:37:15 -0300

    Criação dos widgets Silver

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,39 +1,61 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:job_timer/app/core/database/database_app.dart';
+import 'package:job_timer/app/entities/project.dart';
+import 'package:job_timer/app/entities/project_status.dart';
+import 'package:job_timer/app/modules/home/widgets/header_project_menu.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+      drawer: Drawer(
+        child: SafeArea(
+          child: ListTile(
+            onTap: _signOut,
+            title: Text('Sair'),
+          ),
+        ),
       ),
-      body: Container(
-          child: Column(
-        children: [
-          ElevatedButton(
-            onPressed: _signOut,
-            child: Text('Deslogar'),
-          )
-        ],
-      )),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              title: const Text('Projeto'),
+              expandedHeight: 100,
+              toolbarHeight: 100,
+              centerTitle: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(25),
+                ),
+              ),
+            ),
+            SliverPersistentHeader(delegate: HeaderProjectMenu(),pinned: true,),
+            SliverList(delegate: SliverChildListDelegate([
+              Container(color: Colors.blue,height: 150,),
+              Container(color: Colors.blue,height: 150,),
+              Container(color: Colors.blue,height: 150,),
+              Container(color: Colors.blue,height: 150,),
+              Container(color: Colors.blue,height: 150,),
+              Container(color: Colors.blue,height: 150,),
+              Container(color: Colors.blue,height: 150,),
+              Container(color: Colors.blue,height: 150,),
+            ]),),
+          ],
+        ),
+      ),
     );
   }
 
   Future<void> _signOut() async {
-    FirebaseAuth.instance.authStateChanges().listen((User? user) async {
-      if (user != null) {
-        await FirebaseAuth.instance.signOut();
-      }
-      Modular.to.navigate('/');
-    });
+    try {
+      await FirebaseAuth.instance.signOut();
+      GoogleSignIn().disconnect();
+    } catch (e) {}
   }
 }
