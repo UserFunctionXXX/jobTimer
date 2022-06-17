e363c44259ac756d5bd3d93c2eb0ec2972a91528
UserFunctionXXX <renangomes09@hotmail.com>
2022-06-16 22:36:09 -0300

    Header do page Home que irá criar os projetos

---
+++ lib/app/modules/home/widgets/header_project_menu.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:job_timer/app/entities/project_status.dart';
+
+class HeaderProjectMenu extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return LayoutBuilder(builder: (context, constrains) {
+      return Container(
+        height: constrains.maxHeight,
+        color: Colors.white,
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(
+              width: constrains.maxWidth * .5,
+              child: DropdownButtonFormField<ProjectStatus>(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    contentPadding: EdgeInsets.all(10),
+                    isCollapsed: true),
+                items: ProjectStatus.values.map(
+                  (e) => DropdownMenuItem(
+                    value: e,
+                    child: Text(e.label),
+                  ),
+                ).toList(),
+                onChanged: (value) {},
+              ),
+            ),
+            SizedBox(
+              width: constrains.maxWidth * .4,
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.add),
+                label: Text('Novo Projeto'),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+
+  @override
+  double get maxExtent => 80;
+
+  @override
+  double get minExtent => 80;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
