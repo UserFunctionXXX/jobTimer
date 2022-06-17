b6e068de82b8f63a261291fb8bedb9c7640a0fa3
UserFunctionXXX <renangomes09@hotmail.com>
2022-06-16 22:34:38 -0300

    Page + Module do login

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -1,44 +1,74 @@
+import 'package:asuka/asuka.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:job_timer/app/modules/login/controller/login_controller.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatelessWidget {
+  final LoginController controller;
 
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
+  const LoginPage({Key? key, required this.controller}) : super(key: key);
 
-class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
 
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0XFF0092B9),
-              Color(0XFF0167B2),
-            ],
+    return BlocListener<LoginController, LoginState>(
+      bloc: controller,
+      listenWhen: (previous, current) => previous.status != current.status,
+      listener: (context, state) {
+        if (state.status == LoginStatus.failure) {
+          final message = state.errorMessage ?? 'Erro ao realizar login';
+          AsukaSnackbar.alert(message).show();
+        }
+      },
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0XFF0092B9),
+                Color(0XFF0167B2),
+              ],
+            ),
           ),
-        ),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/logo.png'),
-              SizedBox(height: screenSize.height * .1),
-              SizedBox(
-                width: screenSize.width * .8,
-                height: 49,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.grey[200]),
-                  onPressed: () {},
-                  child: Image.asset('assets/images/google.png'),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/logo.png'),
+                SizedBox(
+                  height: screenSize.height * .1,
                 ),
-              ),
-            ],
+                SizedBox(
+                  width: screenSize.width * .8,
+                  height: 49,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      controller.signIn();
+                    },
+                    style: ElevatedButton.styleFrom(primary: Colors.grey[200]),
+                    child: Image.asset('assets/images/google.png'),
+                  ),
+                ),
+                BlocSelector<LoginController, LoginState, bool>(
+                  bloc: controller,
+                  selector: (state) => state.status == LoginStatus.loading,
+                  builder: (context, show) {
+                    return Visibility(
+                      visible: show,
+                      child: const Padding(
+                        padding: EdgeInsets.only(top: 15.0),
+                        child: Center(
+                          child: CircularProgressIndicator.adaptive(
+                            backgroundColor: Colors.white,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
